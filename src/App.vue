8da7408303e5ddfa010f69739d7b74e0ddc9f9d1
alhiefikri <alhiepedia@gmail.com>
2026-03-30 22:53:45 +0800

    learn: event listener and methods

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <script setup>
 import { ref, reactive } from "vue";
+
 // Bagian script setup adalah tempat kita mendefinisikan logic dan data.
 // Variabel 'nama' ini akan kita panggil di template menggunakan interpolation.
-const nama = "Alhie Fikri";
 console.log("Hello, Gua Alhie");
+// const nama = "Alhie Fikri";
+function greeting(nama) {
+  return `Hello, gua ${nama}`
+}
 
 // deklarative rendering
 const counter = reactive({ count: 0 });
-counter.count++;
+function increment(number) {
+  counter.count = counter.count + number;
+}
 
 const message = ref("Hi");
 
 message.value = "Hello";
 
-const link = "https://vuejs.org"
+const link = "https://vuejs.org";
 </script>
 
 <template>
   <div class="container">
-    <!-- 
-      Interpolation (Mustache Syntax) menggunakan double curly braces {{ }} 
+    <!--
+      Interpolation (Mustache Syntax) menggunakan double curly braces {{ }}
       Ini merender nilai dari variabel JavaScript (nama) langsung ke dalam HTML.
     -->
-    <h1>Hello, Gua {{ nama }}</h1>
-
+    <h1>{{ greeting('Alhie') }}</h1>
 
     <p>Gua belajar vue pada sesi kali ini</p>
   </div>
   <div class="container">
-    <p>{{ counter.count }}</p>
+    <button @click="increment(2)">Count is {{ counter.count }}</button>
     <p>{{ message }}</p>
     <p>{{ message.split("").reverse().join("") }}</p>
     <a :href="link">Kunjungi Vue pakai vbind</a>
