9edd9c063afb01661f11d1af21a7669ea8bb9f19
alhiefikri <alhiepedia@gmail.com>
2026-03-30 22:12:30 +0800

    learn: deklarative rendering

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <script setup>
-console.log("Hello, Gua Alhie");
+import { ref, reactive } from "vue";
+// Bagian script setup adalah tempat kita mendefinisikan logic dan data.
+// Variabel 'nama' ini akan kita panggil di template menggunakan interpolation.
 const nama = "Alhie Fikri";
+console.log("Hello, Gua Alhie");
+
+// deklarative rendering
+const counter = reactive({ count: 0 });
+counter.count++;
+
+const message = ref("Hi");
+message.value = "Hello";
 </script>
 
 <template>
   <div class="container">
+    <!-- 
+      Interpolation (Mustache Syntax) menggunakan double curly braces {{ }} 
+      Ini merender nilai dari variabel JavaScript (nama) langsung ke dalam HTML.
+    -->
     <h1>Hello, Gua {{ nama }}</h1>
+
     <p>Gua belajar vue pada sesi kali ini</p>
+  </div>
+  <div class="container">
+    <p>{{ counter.count }}</p>
+    <p>{{ message }}</p>
+    <p>{{ message.split("").reverse().join("") }}</p>
   </div>
 </template>
 
