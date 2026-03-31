41d18fed5e28b8330d733a710a67181720963489
alhiefikri <alhiepedia@gmail.com>
2026-03-31 12:06:25 +0800

    learn: scoped style

--- src/components/greeting/HelloWord.vue
+++ src/components/greeting/HelloWord.vue
@@ -1,9 +1,9 @@
 <template>
-  <h1>Hello World !</h1>
+  <div>
+    <h1 class="container">Hello World !</h1>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped></style>
