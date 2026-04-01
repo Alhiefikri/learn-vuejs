fc09f6849e9b6923bceab929abe5d9a0aecb4407
alhiefikri <alhiepedia@gmail.com>
2026-04-01 20:20:28 +0800

    learn: Teleport

--- src/components/pages/Settings.vue
+++ src/components/pages/Settings.vue
@@ -2,23 +2,43 @@
   <div class="settings-page">
     <h1>⚙️ Settings Page</h1>
     <p>Settings juga di-cache dengan Home!</p>
-    
+
     <div style="margin-top: 20px;">
       <label>
         <input type="checkbox" v-model="notifications" />
         Enable Notifications
       </label>
     </div>
-    
+
     <p style="margin-top: 10px;">
       Checkbox state: <strong>{{ notifications ? 'ON' : 'OFF' }}</strong>
+
+
     </p>
+    <div>
+      <button @click="show = true">show modal</button>
+      <Teleport to="body">
+        <div v-if="show" class="overlay">
+          <div class="modal">
+            <p>ini modal</p>
+            <button @click="show = false">close</button>
+          </div>
+
+        </div>
+      </Teleport>
+
+    </div>
   </div>
+
+
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { onActivated, onDeactivated } from 'vue'
+
+const show = ref(false);
+
 
 const notifications = ref(false)
 
@@ -42,5 +62,20 @@ onDeactivated(() => {
 
 .settings-page h1 {
   color: #f59e0b;
+}
+
+.overlay {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, .6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
 }
 </style>
