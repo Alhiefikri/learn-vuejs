1acdbe8a1b2c278f0445fb871359c909f036cacd
alhiefikri <alhiepedia@gmail.com>
2026-04-01 19:30:01 +0800

    learn: compasables useFetch

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -1,6 +1,22 @@
+<script setup>
+import useFetch from '@/composables/useFetch';
+
+const { data, error, loading } = useFetch('https://jsonplaceholder.typicode.com/posts')
+
+console.log(data)
+</script>
+
 <template>
   <div>
     <h1>Dashboard Page</h1>
+    <div>
+      <div v-if="loading">Loading...</div>
+      <div v-else-if="error">Error : {{ error }}</div>
+      <!-- <pre v-else>{{ data }}</pre> -->
+      <div v-for="post in data">
+        <p>{{ post.title }}</p>
+      </div>
+    </div>
     <router-view />
   </div>
 </template>
